9547a6bc0244889b3766e61bedd067e1386dbea9
Adithya T <86908747+Adithya893@users.noreply.github.com>
2023-02-19 19:38:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout <<"hello, Iam Adithya \n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
